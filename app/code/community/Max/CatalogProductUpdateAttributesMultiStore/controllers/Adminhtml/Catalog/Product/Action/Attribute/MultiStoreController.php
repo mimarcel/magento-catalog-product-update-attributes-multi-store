411bd9d78c8ef485d0d0796b62c7a608c3a85289
Marcel Moldovan <marcel.moldovan@vaimo.com>
2018-07-17 11:48:39 +0200

    Add new controller action

---
+++ app/code/community/Max/CatalogProductUpdateAttributesMultiStore/controllers/Adminhtml/Catalog/Product/Action/Attribute/MultiStoreController.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @see Mage_Adminhtml_Catalog_Product_Action_AttributeController
+ */
+class Max_CatalogProductUpdateAttributesMultiStore_Adminhtml_Catalog_Product_Action_Attribute_MultiStoreController
+    extends Mage_Adminhtml_Controller_Action
+{
+    public function editAction()
+    {
+
+    }
+}
