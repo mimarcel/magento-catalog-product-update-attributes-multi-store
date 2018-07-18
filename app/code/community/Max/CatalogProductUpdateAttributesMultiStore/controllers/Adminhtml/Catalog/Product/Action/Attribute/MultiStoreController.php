1621d0d5be11d077f7d52b499724e35458481e41
Marcel Moldovan <marcel.moldovan@vaimo.com>
2018-07-18 15:20:38 +0200

    Save attributes data

--- app/code/community/Max/CatalogProductUpdateAttributesMultiStore/controllers/Adminhtml/Catalog/Product/Action/Attribute/MultiStoreController.php
+++ app/code/community/Max/CatalogProductUpdateAttributesMultiStore/controllers/Adminhtml/Catalog/Product/Action/Attribute/MultiStoreController.php
@@ -59,6 +59,65 @@ class Max_CatalogProductUpdateAttributesMultiStore_Adminhtml_Catalog_Product_Act
         $this->getResponse()->setBody($response->toJson());
     }
 
+    public function saveAction()
+    {
+        try {
+            $this->_validateProducts();
+            $attributesData = $this->getRequest()->getParam('attributes', array());
+            if ($attributesData) {
+                $dateFormat = Mage::app()->getLocale()->getDateFormat(Mage_Core_Model_Locale::FORMAT_TYPE_SHORT);
+
+                $productsData = array();
+                foreach ($attributesData as $attributeCode => $storesValues) {
+                    $attribute = Mage::getSingleton('eav/config')
+                        ->getAttribute(Mage_Catalog_Model_Product::ENTITY, $attributeCode);
+                    if (!$attribute->getAttributeId()) {
+                        Mage::throwException($this->_getHelper()->__('Unknown attribute %1', $attributeCode));
+                    }
+
+                    foreach ($storesValues as $storeId => $productsValues) {
+                        foreach ($productsValues as $productId => $productValue) {
+                            if ($attribute->getBackendType() == 'datetime') {
+                                if (!empty($productValue)) {
+                                    $filterInput    = new Zend_Filter_LocalizedToNormalized(array(
+                                        'date_format' => $dateFormat
+                                    ));
+                                    $filterInternal = new Zend_Filter_NormalizedToLocalized(array(
+                                        'date_format' => Varien_Date::DATE_INTERNAL_FORMAT
+                                    ));
+                                    $productValue = $filterInternal->filter($filterInput->filter($productValue));
+                                } else {
+                                    $productValue = null;
+                                }
+                                $productsData[$productId][$storeId][$attributeCode] = $productValue;
+                            } else {
+                                $productsData[$productId][$storeId][$attributeCode] = $productValue;
+                            }
+                        }
+                    }
+                }
+
+                foreach ($productsData as $productId => $storesValues) {
+                    foreach ($storesValues as $storeId => $attributesValues) {
+                        Mage::getSingleton('catalog/product_action')
+                            ->updateAttributes(array($productId), $attributesValues, $storeId);
+                        // @todo Improve performance by rewriting updateAttributes to support multiple products ids and stores ids
+                    }
+                }
+            }
+
+            $this->_getSession()->addSuccess(
+                $this->__('Total of %d record(s) were updated', count($this->_getHelper()->getProductIds()))
+            );
+        } catch (Mage_Core_Exception $e) {
+            $this->_getSession()->addError($e->getMessage());
+        } catch (Exception $e) {
+            $this->_getSession()->addException($e, $this->__('An error occurred while updating the product(s) attributes.'));
+        }
+
+        $this->_redirect('*/catalog_product/');
+    }
+
     /**
      * Validate selection of products for massupdate
      */
